5e2b056e6d2a1719f8482a39078ea59e7e6638de
Pavlo Syzonenko <pavlo.syzonenko@gmail.com>
2023-07-08 23:50:45 +0300

    feat: classes were removed

--- src/data/client.ts
+++
@@ -1,9 +0,0 @@
-import { DynamoDB } from "aws-sdk";
-
-let client: DynamoDB.DocumentClient | null = null;
-
-export const getClient = (): DynamoDB.DocumentClient => {
-  if (client) return client;
-  client = new DynamoDB.DocumentClient();
-  return client;
-};
