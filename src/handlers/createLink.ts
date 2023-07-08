cb2ccefbe4f70f8edb78b3045f677a828cc11d18
Pavlo Syzonenko <pavlo.syzonenko@gmail.com>
2023-07-08 15:45:55 +0300

    feat: clientLink endpoint
    change aws-sdk v2 to v3.
    update logic of clientLink handler, to newer version,
    now it saving item to dynamodb
    
    aws-sdk v2 no longer supported by nodejs18.x runtime

--- src/handlers/createLink.ts
+++ src/handlers/createLink.ts
@@ -1,17 +1,38 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
-import { Link, createLink } from "../data/link";
+
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+const client = new DynamoDBClient({});
+import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+const ddbDocClient = DynamoDBDocumentClient.from(client);
+import { PutCommand } from "@aws-sdk/lib-dynamodb";
 
 export const main = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const { longAlias, lifeTime } = JSON.parse(event.body as string);
+  try {
+    const reqBody = JSON.parse(event.body as string);
 
-  const link = new Link(longAlias, lifeTime);
-  await createLink(link);
+    const link = {
+      ...reqBody,
+      PK: "pk",
+      SK: "sk",
+    };
 
-  const response = {
-    statusCode: 201,
-    headers: { "content-type": "application/json" },
-    body: JSON.stringify({ link }),
-  };
+    await ddbDocClient.send(
+      new PutCommand({
+        TableName: process.env.TABLE_NAME,
+        Item: link,
+      }),
+    );
 
-  return response;
+    return {
+      statusCode: 201,
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({ link }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({ error }),
+    };
+  }
 };
