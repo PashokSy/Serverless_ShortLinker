5064504800a0fc69b7d98bdccddaf36169656349
Pavlo Syzonenko <pavlo.syzonenko@gmail.com>
2023-07-09 21:35:22 +0300

    feat: token keys store method
    change method of string private and public key;
    add decryptToken method to decrypt token and take a payload out of it

--- src/util/token.ts
+++ src/util/token.ts
@@ -1,35 +1,46 @@
 import * as jose from "jose";
-import { appendFile } from "node:fs";
+import { createSecret, getSecret } from "./secret";
+
+const ALG = "RSA-OAEP-256";
 
 export const genToken = async (item: Record<string, unknown>): Promise<string> => {
   try {
-    const ALG = "RSA-OAEP-256";
-    let _privateKey = null;
-    let _publicKey = null;
+    let _publicKey = await getSecret("PUBLIC_KEY");
 
-    if (!process.env.PUBLIC_KEY || !process.env.PRIVATE_KEY) {
+    if (!_publicKey) {
       const { publicKey, privateKey } = await jose.generateKeyPair(ALG);
-      _privateKey = privateKey;
-      _publicKey = publicKey;
 
       const pkcs8Pem = await jose.exportPKCS8(privateKey);
       const spkiPem = await jose.exportSPKI(publicKey);
 
-      let data = `${"PUBLIC_KEY"}: "${spkiPem}"\n${"PRIVATE_KEY"}: "${pkcs8Pem}"`;
-
-      appendFile("./env.yml", data, (err) => {
-        if (err) throw err;
-      });
-    } else {
-      _privateKey = await jose.importPKCS8(process.env.PRIVATE_KEY, ALG);
-      _publicKey = await jose.importSPKI(process.env.PUBLIC_KEY, ALG);
+      await createSecret("PRIVATE_KEY", pkcs8Pem);
+      await createSecret("PUBLIC_KEY", spkiPem);
     }
+
+    _publicKey = await getSecret("PUBLIC_KEY");
+
+    const publicKeyLike = await jose.importSPKI(_publicKey as string, ALG);
 
     const jweToken = await new jose.CompactEncrypt(new TextEncoder().encode(JSON.stringify(item)))
       .setProtectedHeader({ alg: ALG, enc: "A256GCM" })
-      .encrypt(_publicKey);
+      .encrypt(publicKeyLike);
 
     return jweToken;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const decryptToken = async (token: string): Promise<string> => {
+  try {
+    const _privateKey = await getSecret("PRIVATE_KEY");
+    const privateKeyLike = await jose.importPKCS8(_privateKey as string, ALG);
+
+    const decryptResult = await jose.compactDecrypt(token, privateKeyLike);
+
+    const data = new TextDecoder().decode(decryptResult.plaintext);
+
+    return data;
   } catch (error) {
     throw error;
   }
