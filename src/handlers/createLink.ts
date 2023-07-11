c9667aefc444d1abf32090be0672298445aa38f1
Pavlo Syzonenko <pavlo.syzonenko@gmail.com>
2023-07-11 15:01:06 +0300

    fix: createLink
    change logic of createLink handler

--- src/handlers/createLink.ts
+++ src/handlers/createLink.ts
@@ -1,39 +1,27 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
-import { randomBytes } from "crypto";
-
-import { getClient } from "../util/client";
-import { PutCommand } from "@aws-sdk/lib-dynamodb";
+import { decryptToken } from "../util/token";
+import { Link, generateShortAlias, saveLink } from "../data/link";
 
 const headers = { "content-type": "application/json" };
 
 export const main = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   try {
     const { longAlias, lifeTime } = JSON.parse(event.body as string);
+    const authorizationToken = event.headers["authorizationtoken"] as string;
+    const authorizerArr = authorizationToken.split(" ");
+    const token = authorizerArr[1];
+    const { email } = JSON.parse(await decryptToken(token));
+    const shortAlias = await generateShortAlias();
+    const link = new Link(email, longAlias, lifeTime, shortAlias);
+    await saveLink(link);
 
-    const shortAlias = randomBytes(4).toString("hex");
-
-    const link = {
-      PK: shortAlias,
-      SK: shortAlias,
-      longAlias,
-      shortAlias,
-      lifeTime,
-      visitCount: 0,
-    };
-
-    const client = getClient();
-
-    await client.send(
-      new PutCommand({
-        TableName: process.env.TABLE_NAME,
-        Item: link,
-      }),
-    );
+    const host = event.headers["host"] as string;
+    const response = host + "/" + shortAlias;
 
     return {
       statusCode: 201,
       headers,
-      body: JSON.stringify({ link }),
+      body: JSON.stringify({ shortLink: response, longLink: longAlias }),
     };
   } catch (error) {
     throw error;
