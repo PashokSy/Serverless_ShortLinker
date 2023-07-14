503b31806bbc6fe06d33a620ef8e497d6d622f6f
Pavlo Syzonenko <pavlo.syzonenko@gmail.com>
2023-07-14 16:00:48 +0300

    fix: deactivator
    deactivator will now correctly deactivate expired links

--- src/handlers/deactivator.ts
+++ src/handlers/deactivator.ts
@@ -1,17 +1,18 @@
-import {} from "aws-lambda";
+import { SQSEvent } from "aws-lambda";
 import { getClient } from "../util/client";
 import { PutCommand, ScanCommand } from "@aws-sdk/lib-dynamodb";
 
+//export const main = async (event): Promise<Handler<SNSEvent | EventBridgeEvent<any, any>>> => {
 export const main = async () => {
   try {
     const client = getClient();
 
     const input = {
       TableName: process.env.TABLE_NAME,
-      FilterExpression: "deactivated <> :deactivated AND deactivateAt <> :deactivateNull",
+      FilterExpression: "attribute_exists(deactivated) AND deactivated <> :deactivated AND deactivateAt <> :Null",
       ExpressionAttributeValues: {
         ":deactivated": true,
-        ":deactivateNull": null,
+        ":Null": null,
       },
     };
     const output = await client.send(new ScanCommand(input));
@@ -21,10 +22,9 @@ export const main = async () => {
     if (typeof linksArr != "undefined" && linksArr.length != 0) {
       for (let i = 0; i < linksArr.length; i++) {
         const currentLink = linksArr[i];
-        const lifetime = currentLink["deactivateAt"] - currentLink["createdAt"];
+        const lifetime = currentLink["deactivateAt"] - Date.now();
         if (lifetime <= 0) {
           currentLink["deactivated"] = true;
-
           client.send(
             new PutCommand({
               TableName: process.env.TABLE_NAME,
