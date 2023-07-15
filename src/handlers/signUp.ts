9452b8c9570112e7529a4f5804a4caf22bf5516f
Pavlo Syzonenko <pavlo.syzonenko@gmail.com>
2023-07-15 21:31:56 +0300

    feat: signUp handler
    add email verification

--- src/handlers/signUp.ts
+++ src/handlers/signUp.ts
@@ -1,12 +1,17 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import { User, saveUser } from "../data/user";
 
+import { SESClient, VerifyEmailAddressCommand } from "@aws-sdk/client-ses";
+
 const headers = { "content-type": "application/json" };
 
 export const main = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   try {
     const { email, password } = JSON.parse(event.body as string);
     const user = new User(email, password);
+
+    const sesClient = new SESClient({});
+    await sesClient.send(new VerifyEmailAddressCommand({ EmailAddress: email }));
 
     const response = await saveUser(user);
 
