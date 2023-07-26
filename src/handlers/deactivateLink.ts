b4d5d3996a96887570d8a3cd058eaf3fd70ac1f1
Pavlo Syzonenko <pavlo.syzonenko@gmail.com>
2023-07-26 21:54:41 +0300

    fix: deactivateLink

--- src/handlers/deactivateLink.ts
+++ src/handlers/deactivateLink.ts
@@ -3,22 +3,30 @@ import { deactivateLink } from "../data/link";
 import { CustomError } from "../error/customError";
 import { errorHandler } from "../error/errorHandler";
 import { constructResponse } from "../util/response";
+import { decryptToken } from "../util/token";
 
 export const main = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   try {
     const { link } = JSON.parse(event.body as string);
+    const { authorizationToken } = event.headers;
+    const authorizerArr = (authorizationToken as string).split(" ");
+    const { email } = JSON.parse(await decryptToken(authorizerArr[1]));
 
-    if (!link) {
-      throw new CustomError(400, "No link provided");
+    const { baseUrl, shortAlias } = splitLink(link);
+
+    if (baseUrl != process.env.BASE_URL) {
+      throw new CustomError(409, "Provided link is invalid");
     }
 
-    const arr = link.split("/");
-    const shortAlias = arr[arr.length - 1];
-
-    await deactivateLink(shortAlias);
+    await deactivateLink(email, shortAlias);
 
     return constructResponse(200, { deactivated: `${link}` });
   } catch (error) {
     return errorHandler(error);
   }
+};
+
+const splitLink = (link: string): Record<string, string> => {
+  const lastIndex = link.lastIndexOf("/");
+  return { baseUrl: link.slice(0, lastIndex + 1), shortAlias: link.slice(lastIndex + 1) };
 };
