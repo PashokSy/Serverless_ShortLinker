d9f5b97b497008627a9cd0f724792bd934a47887
Pavlo Syzonenko <pavlo.syzonenko@gmail.com>
2023-07-26 20:40:38 +0300

    fix: authorizer

--- src/auth/authorizer.ts
+++ src/auth/authorizer.ts
@@ -1,39 +1,43 @@
-import { APIGatewayAuthorizerResult, PolicyDocument, APIGatewayRequestAuthorizerEventV2 } from "aws-lambda";
+import { APIGatewayAuthorizerResult, PolicyDocument, APIGatewayRequestAuthorizerEvent } from "aws-lambda";
 import { decryptToken } from "../util/token";
 import { User, getUser } from "../data/user";
 
-export const main = async (event: APIGatewayRequestAuthorizerEventV2): Promise<APIGatewayAuthorizerResult> => {
+export const main = async (event: APIGatewayRequestAuthorizerEvent): Promise<APIGatewayAuthorizerResult> => {
   try {
     const headers = event.headers;
+    const resource = event.methodArn;
 
-    if (!headers) return generatePolicy("undefined", "Deny");
+    if (!headers) return generatePolicy("undefined", "Deny", resource);
 
     const { authorizationToken } = headers;
 
-    if (!authorizationToken) return generatePolicy("undefined", "Deny");
+    if (!authorizationToken) return generatePolicy("undefined", "Deny", resource);
 
     const authorizerArr = authorizationToken.split(" ");
-    const token = authorizerArr[1];
 
     if (authorizerArr.length != 2 || authorizerArr[0] != "Bearer" || authorizerArr[1].length === 0) {
-      return generatePolicy("undefined", "Deny");
+      return generatePolicy("undefined", "Deny", resource);
     }
 
-    const payload = await decryptToken(token);
+    const payload = await decryptToken(authorizerArr[1]);
     const user = User.fromItem(JSON.parse(payload));
 
     if (!(await getUser(user))) {
-      return generatePolicy("undefined", "Deny");
+      return generatePolicy(user.email, "Deny", resource);
     }
 
     // authorization success
-    return generatePolicy(user.email, "Allow");
+    return generatePolicy(user.email, "Allow", resource);
   } catch (error) {
-    return generatePolicy("undefined", "Deny");
+    throw Error("Unauthorized");
   }
 };
 
-const generatePolicy = async (principalId: string, effect: string): Promise<APIGatewayAuthorizerResult> => {
+const generatePolicy = async (
+  principalId: string,
+  effect: string,
+  resource: string,
+): Promise<APIGatewayAuthorizerResult> => {
   const policyDocument = {} as PolicyDocument;
   if (effect) {
     policyDocument.Version = "2012-10-17";
@@ -41,7 +45,7 @@ const generatePolicy = async (principalId: string, effect: string): Promise<APIG
     const statementOne: any = {};
     statementOne.Action = "execute-api:Invoke";
     statementOne.Effect = effect;
-    statementOne.Resource = "*";
+    statementOne.Resource = resource;
     policyDocument.Statement[0] = statementOne;
   }
 
