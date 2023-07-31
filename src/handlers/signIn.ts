26b57d801086561256a01bed1c119dcc22af3763
Pavlo Syzonenko <pavlo.syzonenko@gmail.com>
2023-07-31 17:12:29 +0300

    fix: signIn

--- src/handlers/signIn.ts
+++ src/handlers/signIn.ts
@@ -1,20 +1,11 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import { User, verifyUser } from "../data/user";
-import { CustomError } from "../error/customError";
 import { errorHandler } from "../error/errorHandler";
 import { constructResponse } from "../util/response";
 
 export const main = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   try {
     const { email, password } = JSON.parse(event.body as string);
-
-    if (!email) {
-      throw new CustomError(400, "No email provided");
-    }
-
-    if (!password) {
-      throw new CustomError(400, "No password provided");
-    }
 
     const user = new User(email, password);
 
