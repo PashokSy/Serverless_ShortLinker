049cd301be898f46668829a9b6aea5d34c6a01e6
Pavlo Syzonenko <pavlo.syzonenko@gmail.com>
2023-07-31 20:29:42 +0300

    fix: deactivationQueue

--- src/handlers/deactivatorQueue.ts
+++ src/handlers/deactivatorQueue.ts
@@ -4,10 +4,10 @@ import { sendEmail } from "../util/sesClient";
 export const main = async (event: SQSEvent) => {
   try {
     for (let i = 0; i < event.Records.length; i++) {
-      const { user, longAlias } = JSON.parse(event.Records[i].body);
+      const { user, longLink, shortAlias } = JSON.parse(event.Records[i].body);
 
       const subject = "Deactivation";
-      const message = `Short link for ${longAlias} was deactivated`;
+      const message = `Short link ${process.env.BASE_URL}${shortAlias} for ${longLink} was deactivated`;
 
       await sendEmail(user, subject, message);
     }
